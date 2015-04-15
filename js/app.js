03ebc96b37147bb93e52821075221b91937e3840
galulex <galulex@gmail.com>
2015-04-15 22:59:04 +0300

    merge scripts

--- js/app.js
+++ js/app.js
@@ -72,3 +72,17 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-50401698-1', 'active-bridge.com');
 ga('send', 'pageview');
+
+// Fonts
+WebFontConfig = {
+  google: { families: [ 'Roboto+Condensed:400italic,700italic,400,700:latin' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
