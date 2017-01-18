017d59b6ce7e6cf4a25eb73caf9f0ccbd72bde7e
daryakuritsyna <darya@acive-bridge.com>
2017-01-18 14:24:21 +0200

    add event listener

--- js/app.js
+++ js/app.js
@@ -46,16 +46,13 @@ function popup(event, e) {
 
 // Google Analytics
 
- var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-50401698-2']);
-  _gaq.push(['_trackPageview']);
-  setTimeout("_gaq.push(['_trackEvent', '15_seconds', 'read'])",15000);
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  ga('create', 'UA-50401698-2', 'auto');
+  ga('send', 'pageview');
 
 // Fonts
 WebFontConfig = {
